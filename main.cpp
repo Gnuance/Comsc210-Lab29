1341a7e229aca6e671fbbd064151e1b30398f0db
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-11-05 14:52:44 -0800

    Proposal drafting: 10 minute timestamp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 // Person object definition
 
 // FUNCTIONS
-// generate random name/age for Person
+// generate Person with random name/age
 // determine recovery/death for Person
 // calculate number of new infections
 
@@ -13,7 +13,19 @@
 
 // MAIN
 // read first and last name data from file (state abbrev: population) && place into global arrays && close file; EXIT if ERROR
-// read state data from file (state abbrev: population) && close file; EXIT if ERROR
 
-// initialize map for each state from file {State<string>: population<int>, infected<list>, recovered<list>, died<list>}
+// read state data from file (state abbrev: population,); EXIT if ERROR
+// for each item from state file --> initialize map element with {State<string>: population<int>, infected<list> = null, recovered<list> = null, died<list> = null}
+// close state data file
+
+// begin simulation of infection events
+// for 52 intervals
+// for each state <map element>
+// CALL: function to determine recovery/death for each infected person
+// place person into list determined by function
+// CALL: function calculate number of new infections
+// if new infections:
+    // CALL: function to generate random Person
+    // add to infected list
+
 
