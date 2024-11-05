cce6959116778ef9b231d631fbe8e7b3bc08a95e
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-11-05 14:22:40 -0800

    Proposal drafting: 10 minute timestamp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// HEADERS
+// data structures
+// Person object definition
+
+// FUNCTIONS
+// generate random name/age for Person
+// determine recovery/death for Person
+// calculate number of new infections 
+
+// MAIN
