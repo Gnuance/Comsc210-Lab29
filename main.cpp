033a5bf407ee566df69b99da737fa27c01e0cc7d
Gnuance <179989680+Gnuance@users.noreply.github.com>
2024-11-05 18:14:48 -0800

    Wireframe drafting: 10 minute timestamp

--- main.cpp
+++ main.cpp
@@ -33,33 +33,38 @@ const string LAST_NAMES[100] = {};
 // END GLOBAL
 
 // MAIN
-// read first and last name data from file (state abbrev: population) && place into global arrays && close file; EXIT if ERROR
+int main()
+{
+    // read first and last name data from file (state abbrev: population) && place into global arrays && close file; EXIT if ERROR
 
-// read state data from file (state abbrev: population,); EXIT if ERROR
-// for each item from state file --> initialize map element with {State<string>: population<int>, infected<list> = null, recovered<list> = null, died<list> = null}
-// close state data file
+    // read state data from file (state abbrev: population,); EXIT if ERROR
+    // for each item from state file --> initialize map element with {State<string>: population<int>, infected<list> = null, recovered<list> = null, died<list> = null}
+    // close state data file
 
-// begin simulation of infection events
-// for 52 intervals
-// for each state <map element>
-// CALL: function to determine recovery/death for each infected person
-// place person into list determined by function
-// CALL: function calculate number of new infections
-// if new infections:
-// CALL: function to generate random Person
-// add to infected list
-// output state summary to console
-// sleep timer to let user read results
-// NEXT: state to process
+    // begin simulation of infection events
+    // for 52 intervals
+    // for each state <map element>
+    // CALL: function to determine recovery/death for each infected person
+    // place person into list determined by function
+    // CALL: function calculate number of new infections
+    // if new infections:
+    // CALL: function to generate random Person
+    // add to infected list
+    // output state summary to console
+    // sleep timer to let user read results
+    // NEXT: state to process
 
-// all states processed, output current snapshot of all states to console
-// sleep timer to let user read summary
-// NEXT: iteration
+    // all states processed, output current snapshot of all states to console
+    // sleep timer to let user read summary
+    // NEXT: iteration
 
-// output summary to console of infected/recovered/dead after 52 intervals
+    // output summary to console of infected/recovered/dead after 52 intervals
 
-// END MAIN
+    // END MAIN
+    return 0;
+}
 
-Person &CreateRandomPerson(){
-
+Person &
+CreateRandomPerson()
+{
 }
